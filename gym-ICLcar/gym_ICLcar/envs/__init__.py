ca847a83262c75d37ae6dda297860fd1d29e6aba
weiye zhao <Caesar@zhaoweiyedeMacBook-Pro.local>
2020-07-03 00:04:32 -0400

    update

--- gym-ICLcar/gym_ICLcar/envs/__init__.py
+++
@@ -1,1 +0,0 @@
-from gym_ICLcar.envs.ICLcar_env import ICLcarEnv
--- gym-ICLcar/setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-
-setup(name='gym_ICLcar',
-      version='0.0.1',
-      install_requires=['gym'],
-)
